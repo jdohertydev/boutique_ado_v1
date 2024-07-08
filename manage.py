bc676101c02658f25b839c83259b2bdaae2fec4c
John Doherty <johndoherty1984@live.com>
2024-07-08 15:10:27 +0000

    Add basic checkout functionality

--- manage.py
+++ manage.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
 import os
 import sys
 
+# Import env.py
+import env
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'boutique_ado.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'your_project_name.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -16,7 +17,6 @@ def main():
             "forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
-
 
 if __name__ == '__main__':
     main()
